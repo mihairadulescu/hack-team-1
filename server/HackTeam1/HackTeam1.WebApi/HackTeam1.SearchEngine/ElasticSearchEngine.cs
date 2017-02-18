d4b767d7152ea136cbab12bc53118ab89789a35e
Alexandru Beu <alexandru.beu@nerdscomputing.com>
2017-02-18 21:35:58 +0200

    add search engine

---
+++ server/HackTeam1/HackTeam1.WebApi/HackTeam1.SearchEngine/ElasticSearchEngine.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HackTeam1.Entities;
+using Nest;
+
+namespace HackTeam1.SearchEngine
+{
+    public class ElasticSearchEngine : IElasticSearchEngine
+    {
+        private readonly ElasticClient _client;
+
+        public ElasticSearchEngine()
+        {
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node).DefaultIndex("document");
+            _client = new ElasticClient(settings);
+
+        }
+
+        public void Index(Document document)
+        {
+
+            _client.Index(document);
+        }
+
+        public IEnumerable<Document> Search(string searchPhrase)
+        {
+
+            var textField = Infer.Field<Document>(z => z.Text, -0.3);
+            var categoryField = Infer.Field<Document>(z => z.Category, 1);
+            var origFileName = Infer.Field<Document>(z => z.OriginalFileName, 1.2);
+            var titleField = Infer.Field<Document>(z => z.Title, 1.5);
+
+            var searchRequest = new SearchRequest<Document>()
+            {
+                Query = new MultiMatchQuery()
+                {
+                    Fields = new[] { textField, categoryField, origFileName, titleField },
+                    Fuzziness = Fuzziness.EditDistance(int.MaxValue),
+                    Query = searchPhrase
+                },
+            };
+            var response = _client.Search<Document>(searchRequest).Hits;
+
+            return response.OrderByDescending(z => z.Score).Select(p => p.Source);
+
+        }
+       
+    }
+}
---
+++ server/HackTeam1/HackTeam1.WebApi/HackTeam1.SearchEngine/IElasticSearchEngine.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using HackTeam1.Entities;
+
+namespace HackTeam1.SearchEngine
+{
+    public interface IElasticSearchEngine
+    {
+        void Index(Document document);
+        IEnumerable<Document> Search(string searchPhrase);
+    }
+}
