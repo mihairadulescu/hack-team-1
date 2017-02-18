e21bb2ebc86b6b596eb52189bcc9dbf32deaa316
geodanila <geo.danila89@gmail.com>
2017-02-18 16:12:04 +0200

    * Added a test app (console) * Added the required packages & data to run tesseract in the OCR system * Added a simple database (uses a XML file) for storage

---
+++ server/HackTeam1/HackTeam1.WebApi/HackTeam1.ConsoleTestApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HackTeam1.Core;
+using HackTeam1.Entities;
+using HackTeam1.OcrSystem;
+
+namespace HackTeam1.ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDocumentOcr ocr = new DocumentOcr();
+
+            
+        }
+
+        private static void SeedDb()
+        {
+            var documentDb = new DocumentDb();
+            documentDb.SaveDocument(new Document
+            {
+                OriginalFileName = "first.pdf",
+                CreatedDate = DateTime.Now
+            });
+            documentDb.SaveDocument(new Document
+            {
+                OriginalFileName = "second.pdf",
+                CreatedDate = DateTime.Now
+            });
+            documentDb.SaveDocument(new Document
+            {
+                OriginalFileName = "third.pdf",
+                CreatedDate = DateTime.Now
+            });
+            documentDb.SaveDocument(new Document
+            {
+                OriginalFileName = "fourth.pdf",
+                CreatedDate = DateTime.Now
+            });
+        }
+    }
+}
