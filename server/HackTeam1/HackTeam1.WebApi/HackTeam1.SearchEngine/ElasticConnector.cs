99319aac8e15f2b5118d4ed2357804575eaf2618
Alexandru Beu <alexandru.beu@nerdscomputing.com>
2017-02-18 14:47:56 +0200

    add document examples

---
+++ server/HackTeam1/HackTeam1.WebApi/HackTeam1.SearchEngine/ElasticConnector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Nest;
+
+namespace HackTeam1.SearchEngine
+{
+    public class ElasticConnector 
+    {
+        public ElasticConnector()
+        {
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+        }
+    }
+}
