d3e220e6651c1d945afb10eb35e3dd3c9a68d431
Daniel Schiau <schiau.daniel@yahoo.com>
2017-02-18 23:21:47 +0200

    WebApi Controller Mock

--- server/HackTeam1/HackTeam1.WebApi/HackTeam1.WebApi/Startup.cs
+++ server/HackTeam1/HackTeam1.WebApi/HackTeam1.WebApi/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Http;
 using Microsoft.Owin;
+using MultipartDataMediaFormatter;
 using Owin;
 
 [assembly: OwinStartup(typeof(HackTeam1.WebApi.Startup))]
@@ -12,6 +14,8 @@ namespace HackTeam1.WebApi
     {
         public void Configuration(IAppBuilder app)
         {
+            GlobalConfiguration.Configuration.Formatters.Add(new FormMultipartEncodedMediaTypeFormatter());
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
     }
